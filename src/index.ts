42c49e9803cba27c69056011f49595c7aaddafc8
faultyblaster <192670544+faultyblaster@users.noreply.github.com>
2025-03-10 09:57:39 -0600

    First commit

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { VsLogger } from './vs-logger';
---
+++ src/vs-logger.ts
@@ -0,0 +1,37 @@
+import * as vscode from 'vscode';
+
+/**
+ * VsLogger implementation for use in vscode extensions, simple, easy to use.
+ */
+export class VsLogger {
+    channel: vscode.OutputChannel | undefined;
+
+    public initializeChannel(name: string) {
+        if (this.channel === undefined) {
+            this.channel = vscode.window.createOutputChannel(name, 'log');
+        }
+        this.channel.show();
+        this.logInfo(`Logger successfully initialized for ${name}`);
+    }
+    public logInfo(message: string): void {
+        let date = new Date(Date.now());
+        this.channel?.appendLine(
+            `[${date.toLocaleString()}] [INFO   ]: ${message}`
+        );
+    }
+    public logWarning(message: string): void {
+        let date = new Date(Date.now());
+        this.channel?.appendLine(
+            `[${date.toLocaleString()}] [WARNING]: ${message}`
+        );
+    }
+    public logError(message: string): void {
+        let date = new Date(Date.now());
+        this.channel?.appendLine(
+            `[${date.toLocaleString()}] [ERROR  ]: ${message}`
+        );
+    }
+    constructor(name: string) {
+        this.initializeChannel(name);
+    }
+}
